0168ad42f2c05ba837847580a917c41b568665c1
Iqbal Tawakkal <iqbaltawakkal@xtremax.com>
2020-07-11 02:31:30 +0700

    footer component

---
+++ components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>
+        <strong>Studio Ghilby Fansite</strong> by Iqbal Tawakkal. Website
+        content provided by
+        <a target="blank" href="https://ghibliapi.herokuapp.com">
+          Ghilbi's API
+        </a>
+      </p>
+    </div>
+  </footer>
+</template>
+
+<style lang="scss">
+a {
+  color: darkgray;
+}
+</style>
